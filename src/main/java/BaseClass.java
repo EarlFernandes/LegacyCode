118a8b3a49c73b856d2ad336ae6abf2a0ed1accc
Earl Fernandes <earl@swarmio.media>
2021-07-12 14:05:34 -0400

    Changes reverted back to scripts working state

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -10,19 +10,14 @@ import java.util.concurrent.TimeUnit;
 public class BaseClass {
 
     WebDriver driver;
-    private CBTAPI api;
 
     @BeforeClass
     public void setupApplication() {
-        String username = System.getenv("CBTUSRNAME").replaceAll("@", "%40");
-        String authkey = System.getenv("CBTAUTH");
-        System.out.println(username);
         WebDriverManager.chromedriver().setup();
         ChromeOptions opt = new ChromeOptions();
         opt.setHeadless(true);
         driver = new ChromeDriver(opt);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        api = new CBTAPI(username, authkey);
 
     }
 
