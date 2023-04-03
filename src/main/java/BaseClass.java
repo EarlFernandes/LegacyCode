6c74bb935c305a39962d4f0a4fee8d2617b5ec5c
Earl Fernandes <earl@swarmio.media>
2023-04-03 17:39:35 -0400

    Rearranged arguments

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -17,15 +17,18 @@ public class BaseClass {
 
         WebDriverManager.chromedriver().setup();
         ChromeOptions opt = new ChromeOptions();
-        opt.addArguments("--window-size=1920,1080");
+        opt.addArguments("no-sandbox");
+        opt.addArguments("--disable-dev-shm-usage");
+//        opt.addArguments("headless");
         opt.addArguments("--disable-extensions");
         opt.addArguments("--proxy-server='direct://'");
         opt.addArguments("--proxy-bypass-list=*");
         opt.addArguments("--remote-allow-origins=*");
+        opt.addArguments("--window-size=1920,1080");
         opt.addArguments("--start-maximized");
-        opt.addArguments("headless");
-        opt.addArguments("--disable-dev-shm-usage");
-        opt.addArguments("no-sandbox"); // https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
+
+
+         // https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
         driver = new ChromeDriver(opt);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
